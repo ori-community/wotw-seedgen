ba6c31b04895e7c34348f3be8d04710c912beda7
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2022-10-16 09:47:04 +0200

    fix warp label parsing

--- wotw_seedgen/src/languages/header/parser/parse_item.rs
+++ wotw_seedgen/src/languages/header/parser/parse_item.rs
@@ -336,7 +336,8 @@ fn parse_create_warp(parser: &mut Parser) -> Result<VCommand, ParseError> {
     let position = parse_v_position(parser)?;
     let label = if parser.current_token().kind == TokenKind::Separator {
         let peeked = parser.peek_token();
-        if matches!(peeked.kind, TokenKind::String { .. }) {
+        let range = peeked.range.clone();
+        if matches!(peeked.kind, TokenKind::Identifier) && parser.read(range) != "mute" {  // <.< string literals when
             parser.next_token();
             let label = parse_string(parser).to_owned();
             Some(label)
