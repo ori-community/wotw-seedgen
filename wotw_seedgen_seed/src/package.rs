37babdaa743b99a725875cd4b4eb355491c51c23
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2024-04-09 18:28:30 +0200

    package as zip with partial zstd compression

--- wotw_seedgen_seed/src/package.rs
+++ wotw_seedgen_seed/src/package.rs
@@ -1,53 +1,57 @@
 use crate::{Result, Seed, FORMAT_VERSION};
-use std::{io::Write, path::Path};
+use std::io::{Seek, Write};
+use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
 impl Seed {
-    pub fn package<W: Write>(&self, obj: &mut W) -> Result<()> {
+    pub fn package<W: Write + Seek>(&self, obj: &mut W) -> Result<()> {
         let mut package = Package::new(obj)?;
         package.append("preload.json", serde_json::to_vec(&self.preload)?)?;
-        package.append("assembly.json", serde_json::to_vec(&self.assembly)?)?;
+        package.append_compressed("assembly.json", serde_json::to_vec(&self.assembly)?)?;
         for (path, data) in &self.assets {
-            package.append(path, data)?;
+            package.append(format!("assets/{path}"), data)?;
         }
         package.finish()?;
         Ok(())
     }
 }
 
-struct Package<W: Write> {
-    builder: tar::Builder<xz2::write::XzEncoder<W>>,
-    header: tar::Header,
+struct Package<W: Write + Seek> {
+    zip: ZipWriter<W>,
+    options: FileOptions,
 }
 
-impl<W: Write> Package<W> {
+impl<W: Write + Seek> Package<W> {
     fn new(obj: W) -> Result<Self> {
-        // TODO choose compression
-        let builder = tar::Builder::new(xz2::write::XzEncoder::new(obj, 9));
-        // let mut builder = tar::Builder::new(brotli::CompressorWriter::new(w, 4096, 11, 22));
-        // let mut builder = tar::Builder::new(w);
-        let header = base_header();
-        let mut package = Self { builder, header };
+        let zip = ZipWriter::new(obj);
+        let options = FileOptions::default()
+            .compression_method(CompressionMethod::Zstd)
+            .compression_level(Some(22));
+
+        let mut package = Self { zip, options };
         package.append("format_version.txt", FORMAT_VERSION)?;
         Ok(package)
     }
 
-    fn append<P: AsRef<Path>, D: AsRef<[u8]>>(&mut self, path: P, data: D) -> Result<()> {
-        let data = data.as_ref();
-        self.header.set_path(path)?;
-        self.header.set_size(data.len().try_into()?);
-        self.header.set_cksum();
-        self.builder.append(&self.header, data)?;
+    fn append<S: Into<String>, D: AsRef<[u8]>>(&mut self, name: S, data: D) -> Result<()> {
+        self.append_with(name.into(), data.as_ref(), FileOptions::default())
+    }
+
+    fn append_compressed<S: Into<String>, D: AsRef<[u8]>>(
+        &mut self,
+        name: S,
+        data: D,
+    ) -> Result<()> {
+        self.append_with(name.into(), data.as_ref(), self.options)
+    }
+
+    fn append_with(&mut self, name: String, data: &[u8], options: FileOptions) -> Result<()> {
+        self.zip.start_file(name, options)?;
+        self.zip.write_all(data)?;
         Ok(())
     }
 
-    fn finish(self) -> Result<()> {
-        self.builder.into_inner()?.finish()?.flush()?;
+    fn finish(mut self) -> Result<()> {
+        self.zip.finish()?;
         Ok(())
     }
-}
-
-fn base_header() -> tar::Header {
-    let mut header = tar::Header::new_old();
-    header.set_mode(0o644);
-    header
 }
