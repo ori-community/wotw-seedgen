5be30a2c86598585e90e645faa184527045eb70f
Timo Schwarzer <timo@schwarzer.dev>
2025-02-28 01:09:53 +0100

    Add automatically_derived attribute to TokenDisplay derive macro

--- wotw_seedgen_derive/src/token_display.rs
+++ wotw_seedgen_derive/src/token_display.rs
@@ -39,6 +39,7 @@ pub fn token_display_impl(input: syn::ItemEnum) -> Result<proc_macro::TokenStrea
         .collect::<Result<Vec<_>>>()?;
 
     Ok(quote! {
+        #[automatically_derived]
         impl std::fmt::Display for #ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 match self {
