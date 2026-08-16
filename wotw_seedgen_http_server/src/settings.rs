5895d59b2868097e982c846d3e883bc4772db4d2
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2026-08-16 20:34:08 +0200

    ALWAYS_INLINE_SNIPPETS env var

--- wotw_seedgen_http_server/src/settings.rs
+++ wotw_seedgen_http_server/src/settings.rs
@@ -1,7 +1,16 @@
+use std::{mem, sync::LazyLock};
+
 use tokio::sync::RwLockReadGuard;
-use wotw_seedgen::data::{UniverseSettings, WorldSettings, assets::SnippetAccess};
+use wotw_seedgen::data::{
+    UniverseSettings, WorldSettings,
+    assets::{InlineSnippets, SnippetAccess},
+    env_or,
+};
 
 use crate::{api::snippets::SnippetOrigin, assets::Cache};
+
+static ALWAYS_INLINE_SNIPPETS: LazyLock<bool> =
+    LazyLock::new(|| env_or("ALWAYS_INLINE_SNIPPETS", false));
 
 pub fn inline_universe_snippets(
     universe_settings: &mut UniverseSettings,
@@ -13,16 +22,29 @@ pub fn inline_universe_snippets(
 }
 
 pub fn inline_world_snippets(world_settings: &mut WorldSettings, cache: &RwLockReadGuard<Cache>) {
-    world_settings
-        .snippets
-        .retain(|identifier| match cache.snippet_info[identifier].origin {
-            SnippetOrigin::ExecutableDir => true,
-            SnippetOrigin::UserDataDir => {
-                world_settings
-                    .inline_snippets
-                    .insert(identifier.clone(), cache.read_snippet(identifier).unwrap());
-
-                false
+    if *ALWAYS_INLINE_SNIPPETS {
+        for identifier in world_settings.snippets.drain(..) {
+            inline_snippet(&mut world_settings.inline_snippets, cache, identifier);
+        }
+    } else {
+        world_settings.snippets.retain_mut(|identifier| {
+            match cache.snippet_info[identifier].origin {
+                SnippetOrigin::ExecutableDir => true,
+                SnippetOrigin::UserDataDir => {
+                    let identifier = mem::take(identifier);
+                    inline_snippet(&mut world_settings.inline_snippets, cache, identifier);
+                    false
+                }
             }
         });
+    }
+}
+
+fn inline_snippet(
+    inline_snippets: &mut InlineSnippets,
+    cache: &RwLockReadGuard<Cache>,
+    identifier: String,
+) {
+    let snippet = cache.read_snippet(&identifier).unwrap();
+    inline_snippets.insert(identifier, snippet);
 }
