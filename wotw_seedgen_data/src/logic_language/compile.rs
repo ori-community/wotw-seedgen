7ab2c7295c0e8d4f1c0e0f32816a198ff5445253
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2026-09-05 18:11:27 +0200

    `GrenadeJump=x` syntax

--- wotw_seedgen_data/src/logic_language/compile.rs
+++ wotw_seedgen_data/src/logic_language/compile.rs
@@ -421,7 +421,16 @@ impl RequirementRepository {
             ]),
             Trick::GrenadeJump => Requirement::and([
                 self.trick.grenade_jump.clone(),
-                Requirement::NonConsumingEnergySkill(Skill::Grenade),
+                Requirement::or([
+                    Requirement::and([
+                        self.difficulty.r#unsafe.clone(),
+                        Requirement::NonConsumingEnergySkill(Skill::Grenade),
+                    ]),
+                    match amount.take() {
+                        None => Requirement::Impossible,
+                        Some(amount) => Requirement::EnergySkill(Skill::Grenade, amount as f32),
+                    },
+                ]),
             ]),
             Trick::SwordJump => Requirement::and([
                 self.trick.sword_jump.clone(),
